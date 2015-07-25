824ab9e55649f2c645d3a96a5074a3dc46ec982f
shaun117 <shaun.mataire@live.com>
2015-07-25 15:00:09 -0500

    App Init

---
+++ HealthCruncher_IOS/App/HealthCruncher/HealthCruncher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthCruncher
+//
+//  Created by Shaun Mataire on 7/25/15.
+//  Copyright (c) 2015 HealthCruncher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
